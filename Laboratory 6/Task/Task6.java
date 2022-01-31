654ee403f94f1620eab68d6891742037a3bdcee7
Denis Koshkarev <koshkarev.denis2903@gmail.com>
2022-01-31 17:38:19 +0300

    Feature. Add 6 lab

---
+++ Laboratory 6/Task/Task6.java
@@ -0,0 +1,8 @@
+public class Task6 {
+
+    public static void main(String[] args) {
+
+        PatientValidator.validate("Laboratory 6/Task/Patients.xml", "Laboratory 6/Task/PatientCheck.xsd");
+
+    }
+}
